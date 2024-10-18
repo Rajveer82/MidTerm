5fc92147de9c56c5e54409ac4d97ff96a9954a3e
Rajveer <gillrajveer487@gmail.com>
2024-10-18 17:08:46 -0400

    API fetch song by id

--- server.js
+++ server.js
@@ -11,13 +11,26 @@ mongoose.connect('mongodb+srv://Rajveer:Rajjo123@cluster0.jdl76.mongodb.net/',
   )
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
-    
+
     app.get('/songs', async (req, res) => {
         try {
           const songs = await Song.find(); // Fetch all songs from MongoDB
           res.status(200).json(songs);
         } catch (err) {
           res.status(500).json({ message: 'Error fetching songs' });
+        }
+      });
+
+      app.get('/songs/:id', async (req, res) => {
+        try {
+          const song = await Song.findOne({ songId: parseInt(req.params.id) }); // Find song by ID
+          if (song) {
+            res.status(200).json(song);
+          } else {
+            res.status(404).json({ message: 'Song not found' });
+          }
+        } catch (err) {
+          res.status(500).json({ message: 'Error fetching song' });
         }
       });
 
