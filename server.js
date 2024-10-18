7a813d2af7e342b8a4937c3e898d163d22f14384
Rajveer <gillrajveer487@gmail.com>
2024-10-18 16:29:06 -0400

    set port and required fs

--- server.js
+++ server.js
@@ -2,3 +2,5 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Song = require('./models/Song');
 const app = express();
+const port = 3000;
+const fs = require('fs');
